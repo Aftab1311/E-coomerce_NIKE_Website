e3929ff8d6fb2880976dc971dc62a59ee76b936e
aftab <ampleaftab@gmail.com>
2023-02-24 20:39:10 +0530

    Navbar added

---
+++ nike/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <>
+     
+       <Navbar/>
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
